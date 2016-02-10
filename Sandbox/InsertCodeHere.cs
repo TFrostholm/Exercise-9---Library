28abf871ab3c1b9242537c808b8a87bcca5ef6fc
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-02-10 21:19:17 +0100

    Added code for InsertCodeHere

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -1,16 +1,38 @@
 ﻿using System;
-
-namespace Sandbox
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Library
 {
-    class InsertCodeHere
+    public class InsertCodeHere
     {
         public void MyCode()
         {
-            // The FIRST line of code should be BELOW this line
+            // The FIRST line of code should be BELOW this line 
+            // Step 2a 
+            Book aBook = new Book("HDFCC#", "Head First C# programming", "Peter Larsen", 59.95);
+            // Step 2b 
+            aBook.PrintInformation();
+            // Step 2c 
+            aBook.BorrowFromLibrary();
+            aBook.PrintInformation();
+            // Step 2d 
+            aBook.ReturnToLibrary();
+            aBook.PrintInformation();
+            // Step 4 
+            aBook.BorrowFromLibrary();
+            aBook.ReturnToLibrary();
+            aBook.BorrowFromLibrary();
+            aBook.ReturnToLibrary();
+            aBook.BorrowFromLibrary();
+            aBook.ReturnToLibrary();
+            aBook.BorrowFromLibrary();
+            aBook.ReturnToLibrary();
 
-            Console.WriteLine("Hello Class");
+            // finally print out information with how many times the student borrow the book in a given period of time 
+            aBook.PrintInformation();
 
-            // The LAST line of code should be ABOVE this line
+            // The LAST line of code should be ABOVE this line 
         }
     }
 }
